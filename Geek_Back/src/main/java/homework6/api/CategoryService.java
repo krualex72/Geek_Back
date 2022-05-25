6b91b7ad5d5926ba87ad9ec405a0bcf7705ad753
krutikov-alex <krutikov-alex@ya.ru>
2022-05-25 19:57:05 +0300

    HW6 in Progress

---
+++ Geek_Back/src/main/java/homework6/api/CategoryService.java
@@ -0,0 +1,13 @@
+package homework6.api;
+
+import homework6.dto.GetCategoryResponse;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+
+public interface CategoryService {
+
+    @GET("categories/{id}")
+    Call<GetCategoryResponse> getCategory(@Path("id") int id);
+}
---
+++ Geek_Back/src/main/java/homework6/api/ProductService.java
@@ -0,0 +1,25 @@
+package homework6.api;
+
+import homework6.dto.Product;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+public interface ProductService {
+
+    @POST("products")
+    Call<Product> createProduct(@Body Product createProductRequestPOST);
+
+    @DELETE("products/{id}")
+    Call<ResponseBody> deleteProduct(@Path("id") int id);
+
+    @PUT("products")
+    Call<Product> modifyProduct(@Body Product modifyProductRequestPOST);
+
+    @GET("products/{id}")
+    Call<Product> getProductById(@Path("id") int id);
+
+    @GET("products")
+    Call<ResponseBody> getProducts();
+
+}
